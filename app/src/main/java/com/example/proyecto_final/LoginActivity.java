c12a15e090eda3f1de05af61fb989f0efb14fea8
AbrahamUmana8427 <84608642+AbrahamUmana8427@users.noreply.github.com>
2022-06-19 20:24:11 -0600

    Update

---
+++ app/src/main/java/com/example/proyecto_final/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.proyecto_final;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+
+
+
+
+
+}
