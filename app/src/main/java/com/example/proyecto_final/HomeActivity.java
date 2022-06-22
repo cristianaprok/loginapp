8069c7fd39c1af556476f67cc22d5e06227ee410
jeim05 <lopezvegajeimmy@gmail.com>
2022-06-21 22:28:14 -0600

    update

--- app/src/main/java/com/example/proyecto_final/HomeActivity.java
+++ app/src/main/java/com/example/proyecto_final/HomeActivity.java
@@ -1,8 +1,13 @@
 package com.example.proyecto_final;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.identity.BeginSignInRequest;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -12,10 +17,39 @@ import com.google.android.gms.common.SignInButton;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
+    public static final String TAG = "GoogleSignIn";
+    TextView tvUserName;
+    TextView tvUserEmail;
+    ImageView userImageView;
+    Button btnSignOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        tvUserName = findViewById(R.id.userName);
+        tvUserEmail = findViewById(R.id.userEmail);
+        userImageView = findViewById(R.id.userImage);
+        btnSignOut = findViewById(R.id.btnLogout);
+
+
+        SharedPreferences preferences = this.getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        String userName = preferences.getString("username","");
+        String userEmail = preferences.getString("useremail", "");
+        String userImageUrl = preferences.getString("userPhoto","");
+
+        tvUserName.setText(userName);
+        tvUserEmail.setText(userEmail);
+        //Glide.with(this).load(userImageUrl).into(userImageView);
+
+        btnSignOut.setOnClickListener(view -> {
+            FirebaseAuth.getInstance().signOut();
+            goToMainActivity();
+        });
+    }
+
+    private void goToMainActivity() {
+        startActivity(new Intent(HomeActivity.this, LoginActivity.class));
     }
 }
