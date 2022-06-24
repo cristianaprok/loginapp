c9c5ecac146227e609fb72db09e41eed2fc5cc80
AbrahamUmana8427 <84608642+AbrahamUmana8427@users.noreply.github.com>
2022-06-24 10:47:29 -0600

    Update Abraham

---
+++ app/src/main/java/com/example/proyecto_final/PantallaDosActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaDosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantall_dos);
+    }
+}
