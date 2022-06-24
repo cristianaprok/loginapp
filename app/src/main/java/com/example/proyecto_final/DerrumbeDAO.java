64ebe6a4bb1f70f91992644ebe5fa5f43c7043a4
jeim05 <lopezvegajeimmy@gmail.com>
2022-06-24 11:42:39 -0600

    UPDATED DAO

--- app/src/main/java/com/example/proyecto_final/DerrumbeDAO.java
+++ app/src/main/java/com/example/proyecto_final/DerrumbeDAO.java
@@ -1,5 +1,33 @@
 package com.example.proyecto_final;
 
-public class DerrumbeDAO {
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
+import java.util.HashMap;
+
+public class DerrumbeDAO {
+    private DatabaseReference databaseReference;
+
+    public DerrumbeDAO() {
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        databaseReference = db.getReference(Derrumbe.class.getSimpleName());
+    }
+
+    public Query get(){
+        return databaseReference;
+    }
+
+    public Task<Void> add (Derrumbe der){
+        return databaseReference.push().setValue(der);
+    }
+
+    public Task<Void> update(String key, HashMap<String,Object> hashMap){
+        return databaseReference.child(key).updateChildren(hashMap);
+    }
+
+    public Task<Void> delete(String key){
+        return databaseReference.child(key).removeValue();
+    }
 }
